ad9400330ee5f3cbe7f21b9f37278849fb902a99
at4U <15776554589@163.com>
2019-09-30 15:41:29 +0800

    zt

--- src/main/java/com/cskaoyan/mall/controller/admin/AuthController.java
+++ src/main/java/com/cskaoyan/mall/controller/admin/AuthController.java
@@ -1,6 +1,7 @@
 package com.cskaoyan.mall.controller;
 
 import com.cskaoyan.mall.service.AuthService;
+import com.cskaoyan.mall.util.Page;
 import com.cskaoyan.mall.vo.BaseRespVo;
 import com.cskaoyan.mall.vo.LoginVo;
 import com.cskaoyan.mall.vo.UserInfo;
@@ -51,4 +52,15 @@ public class AuthController {
         userInfo.setRoles(roles);
         return BaseRespVo.success(userInfo);
     }
+
+
+    /**
+     * 系统管理分页
+     *
+     */
+      @RequestMapping("admin/admin/list")
+       public  BaseRespVo showByPage(Page page){
+
+      }
+
 }
