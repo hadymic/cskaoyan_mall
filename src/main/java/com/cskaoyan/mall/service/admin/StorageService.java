ae82a6369dae7999ce3184790ac226cfce50d88b
Hadymic <hadymic@gmail.com>
2019-10-02 09:10:22 +0800

    author:hadymic msg:add picupload

--- src/main/java/com/cskaoyan/mall/service/admin/StorageService.java
+++ src/main/java/com/cskaoyan/mall/service/admin/StorageService.java
@@ -3,6 +3,9 @@ package com.cskaoyan.mall.service.admin;
 import com.cskaoyan.mall.bean.Storage;
 import com.cskaoyan.mall.util.ListBean;
 import com.cskaoyan.mall.util.Page;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface StorageService {
     ListBean<Storage> queryStorage(String key, String name, Page page);
@@ -11,4 +14,5 @@ public interface StorageService {
 
     void update(Storage storage);
 
+    Storage createPic(MultipartFile file) throws IOException;
 }
