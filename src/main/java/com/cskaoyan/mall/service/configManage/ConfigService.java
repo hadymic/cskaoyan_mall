f5b0ef7a65db5625bd114697a3a2e6b341bec9a5
zjd <1604660722@qq.com>
2019-10-02 22:48:53 +0800

    evening

--- src/main/java/com/cskaoyan/mall/service/configManage/ConfigService.java
+++ src/main/java/com/cskaoyan/mall/service/configManage/ConfigService.java
@@ -1,4 +1,25 @@
 package com.cskaoyan.mall.service.configManage;
 
+import com.cskaoyan.mall.util.Page;
+import com.cskaoyan.mall.vo.config.ExpressVo;
+import com.cskaoyan.mall.vo.config.MallConfigVo;
+import com.cskaoyan.mall.vo.config.OrderVo;
+import com.cskaoyan.mall.vo.config.WxVo;
+
+import java.util.Map;
+
 public interface ConfigService {
+    boolean experess(ExpressVo expressVo);
+    Map experess();
+
+    MallConfigVo getMall();
+    boolean updateMall(MallConfigVo vo);
+
+    OrderVo getOredr();
+
+    boolean getOredr(OrderVo orderVo);
+
+    WxVo getWx();
+
+    boolean updateWx(WxVo wxVo);
 }
--- src/main/java/com/cskaoyan/mall/service/userserver/UserManageService.java
+++ src/main/java/com/cskaoyan/mall/service/userserver/UserManageService.java
@@ -1,10 +1,11 @@
 package com.cskaoyan.mall.service.userserver;
 
+import com.cskaoyan.mall.bean.User;
 import com.cskaoyan.mall.util.ListBean;
 import com.cskaoyan.mall.util.Page;
 
 import java.util.List;
 
 public interface UserManageService {
-    ListBean getDispalyList(Page page);
+    ListBean getDispalyList(Page page,String username, String mobile);
 }
