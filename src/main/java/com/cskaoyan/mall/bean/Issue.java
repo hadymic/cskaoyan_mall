5ea657136e0c545fd81876585b49d8fb35294b06
jszza <myangj@163.com>
2019-10-03 16:42:44 +0800

    author: jszza msg:change nothing just for pull

--- src/main/java/com/cskaoyan/mall/bean/Issue.java
+++ src/main/java/com/cskaoyan/mall/bean/Issue.java
@@ -1,5 +1,7 @@
 package com.cskaoyan.mall.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class Issue {
@@ -9,8 +11,10 @@ public class Issue {
 
     private String answer;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date addTime;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     private Boolean deleted;
--- src/main/java/com/cskaoyan/mall/service/goods/impl/CommentServiceImpl.java
+++ src/main/java/com/cskaoyan/mall/service/goods/impl/CommentServiceImpl.java
@@ -6,14 +6,17 @@ import com.cskaoyan.mall.service.goods.CommentService;
 import com.cskaoyan.mall.util.ListBean;
 import com.cskaoyan.mall.util.Page;
 import com.cskaoyan.mall.util.PageUtils;
+import com.cskaoyan.mall.vo.ordermanagement.ReplyVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 @Service
 public class CommentServiceImpl implements CommentService {
     @Autowired
     CommentMapper commentMapper;
+
     @Override
     public ListBean queryComment(Page page,Comment comment) {
         PageUtils.startPage(page);
@@ -24,6 +27,15 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public void deleteComment(Comment comment) {
         commentMapper.updateCommentDeleted(comment.getId());
+    }
+
+    @Override
+    public int updateCommentReply(ReplyVo replyVo) {
+        /*Comment comment = new Comment();
+        comment.setUpdateTime(new Date());
+
+        commentMapper.updateByPrimaryKeySelective();*/
+        return 0;
     }
 
 }
