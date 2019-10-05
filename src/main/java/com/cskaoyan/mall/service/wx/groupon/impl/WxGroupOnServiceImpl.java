3646e28311237272e3ef2278b0ae44690098374e
Zeng-jz <1017055889@qq.com>
2019-10-05 18:07:38 +0800

    author:Zeng-jz msg:团购列表，首页完善

---
+++ src/main/java/com/cskaoyan/mall/service/wx/groupon/impl/WxGroupOnServiceImpl.java
@@ -0,0 +1,35 @@
+package com.cskaoyan.mall.service.wx.groupon.impl;
+
+import com.cskaoyan.mall.bean.Goods;
+import com.cskaoyan.mall.mapper.GoodsMapper;
+import com.cskaoyan.mall.mapper.GrouponRulesMapper;
+import com.cskaoyan.mall.service.wx.groupon.WxGroupOnService;
+import com.cskaoyan.mall.util.Page;
+import com.cskaoyan.mall.util.PageUtils;
+import com.cskaoyan.mall.util.WxListBean;
+import com.cskaoyan.mall.vo.wx.home.GrouponVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WxGroupOnServiceImpl implements WxGroupOnService {
+
+    @Autowired
+    GrouponRulesMapper grouponRulesMapper;
+    @Autowired
+    GoodsMapper goodsMapper;
+
+    @Override
+    public WxListBean selectByPage(Page page) {
+        PageUtils.startPage(page);
+        List<GrouponVo> grouponVoList = grouponRulesMapper.selectAll();
+        for (GrouponVo grouponVo : grouponVoList) {
+            Goods goods = goodsMapper.selectById(grouponVo.getGoods_id());
+            grouponVo.setGoods(goods);
+        }
+        WxListBean<GrouponVo> grouponVoWxListBean = PageUtils.wxPage(grouponVoList);
+        return grouponVoWxListBean;
+    }
+}
