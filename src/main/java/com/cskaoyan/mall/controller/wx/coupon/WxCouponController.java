46553fd694cebbf5c7503a63306f0790a7ed171e
at4U <15776554589@163.com>
2019-10-06 23:58:18 +0800

    zt

--- src/main/java/com/cskaoyan/mall/controller/wx/coupon/WxCouponController.java
+++ src/main/java/com/cskaoyan/mall/controller/wx/coupon/WxCouponController.java
@@ -88,4 +88,8 @@ public class WxCouponController {
 
 
 
+
+
+
+
 }
---
+++ src/main/java/com/cskaoyan/mall/vo/wx/FeedbackVo.java
@@ -0,0 +1,128 @@
+package com.cskaoyan.mall.vo.wx;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+public class FeedbackVo {
+    private Integer id;
+
+    private Integer userId;
+
+    private String username;
+
+    private String mobile;
+
+    private String feedType;
+
+    private String content;
+
+    private Integer status;
+
+    private Boolean hasPicture;
+
+    private String[]  picUrls;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date addTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
+
+    private Boolean deleted;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public String getFeedType() {
+        return feedType;
+    }
+
+    public void setFeedType(String feedType) {
+        this.feedType = feedType == null ? null : feedType.trim();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Boolean getHasPicture() {
+        return hasPicture;
+    }
+
+    public void setHasPicture(Boolean hasPicture) {
+        this.hasPicture = hasPicture;
+    }
+
+    public String[] getPicUrls() {
+        return picUrls;
+    }
+
+    public void setPicUrls(String[] picUrls) {
+        this.picUrls = picUrls;
+    }
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+}
