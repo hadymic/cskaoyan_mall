13fe863f459475086a1b27abe659ead2f7ea70b0
stark_h <514841961@qq.com>
2019-10-06 19:06:50 +0800

    author:hjl msg:专题部分

--- src/main/java/com/cskaoyan/mall/service/wx/topic/WxTopicService.java
+++ src/main/java/com/cskaoyan/mall/service/wx/topic/WxTopicService.java
@@ -1,11 +1,15 @@
 package com.cskaoyan.mall.service.wx.topic;
 
+import com.cskaoyan.mall.bean.Topic;
 import com.cskaoyan.mall.util.Page;
 
+import java.util.List;
 import java.util.Map;
 
 public interface WxTopicService {
     Map<String, Object> queryAllTopic(Page page);
 
     Map<String, Object> queryTopicDetail(int id);
+
+    List<Topic> queryRelatedTopic(int id);
 }
