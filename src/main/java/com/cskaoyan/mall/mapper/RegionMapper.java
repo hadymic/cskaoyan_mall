97be6546876fb086406ebe9a4616b600dea411fd
stark_h <514841961@qq.com>
2019-10-08 17:30:55 +0800

    author:msg:处理冲突

--- src/main/java/com/cskaoyan/mall/mapper/RegionMapper.java
+++ src/main/java/com/cskaoyan/mall/mapper/RegionMapper.java
@@ -1,6 +1,6 @@
 package com.cskaoyan.mall.mapper;
 
-import com.cskaoyan.mall.bean.Address;
+
 import com.cskaoyan.mall.bean.Region;
 import org.apache.ibatis.annotations.Param;
 
@@ -23,10 +23,10 @@ public interface RegionMapper {
 
     List selectById(@Param("pid")int pid);
 
-<<<<<<< HEAD
+
     Region selectByCode(@Param("code") int code);
 
-=======
+
     List<Region> selectRegions();
->>>>>>> b561ea635039713e641b0a54b96eb702d26e95e7
+
 }
